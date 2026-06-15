c4e57dbefdb7fa61bb52bcb61f39e79cf3b180de
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-15 20:45:30 -0300

    Atualização

--- Atividade-Hospital/src/App.jsx
+++ Atividade-Hospital/src/App.jsx
@@ -1,16 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Componentes Estruturais Fixos
-import Header from './assets/components/Header/index.jsx'; 
-import Footer from './assets/components/Footer/index.jsx'; 
+import Header from './components/Header'; 
+import Footer from './components/Footer'; 
 
-// Suas Páginas Prontas
 import Home from './pages/home/index.jsx';
 import QuemSomos from './pages/quem-somos/index.jsx';
 import Servicos from './pages/servicos/index.jsx';
 import Contato from './pages/Contato'; 
-import Blog from './pages/blog'; 
-import CorpoClinico from './pages/corpo-clinico'; // IMPORTADO AQUI EM MINÚSCULO!
+import Blog from './pages/blog/index.jsx'; // 1. IMPORTANTE: Importe o Blog da sua dupla aqui!
 
 import './index.css';
 
@@ -25,8 +22,11 @@ export default function App() {
           <Route path="/quem-somos" element={<QuemSomos />} />
           <Route path="/servicos" element={<Servicos />} />
           <Route path="/contato" element={<Contato />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/corpo-clinico" element={<CorpoClinico />} /> {/* ROTA REAL DEFINIDA! */}
+          
+          {/* 2. CORREÇÃO AQUI: Mudamos de <Home /> para <Blog /> */}
+          <Route path="/blog" element={<Blog />} /> 
+          
+          <Route path="/corpo-clinico" element={<Home />} />
         </Routes>
       </main>
 
